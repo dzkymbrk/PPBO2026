ed01d2f8b0a4513174d16c94bd2271a305c6082c
DZAKY MUBARAK <dzakymubarak@example.com>
2026-09-18 15:50:07 +0700

    Praktikum 5D - Menggunakan Autoload dengan Composer

--- Public/sepuluh.php
+++ Public/sepuluh.php
@@ -1,7 +1,5 @@
 <?php
-
-require_once __DIR__ . '/../app/Site/Admin/HomePage.php';
-require_once __DIR__ . '/../app/Site/Admin/ManajemenArtikel.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use App\Site\Admin\HomePage;
 use App\Site\Admin\ManajemenArtikel;
